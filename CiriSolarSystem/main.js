d51b4dd8830f673d1d8091b99984c8cac7259bba
Rodolfo Rangel Rodrigues <rrrodrigues1@stefanini.com>
2021-11-01 21:07:55 -0300

    projeto finalizado

--- CiriSolarSystem/main.js
+++ CiriSolarSystem/main.js
@@ -3,12 +3,6 @@ import * as THREE from 'three';
 import { EffectComposer } from "/node_modules/three/examples/jsm/postprocessing/EffectComposer.js";
 import { RenderPass } from "/node_modules/three/examples/jsm/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "/node_modules/three/examples/jsm/postprocessing/UnrealBloomPass.js";
-import { FontLoader } from "/node_modules/three/examples/jsm/loaders/FontLoader.js";
-import { TextGeometry } from "/node_modules/three/examples/jsm/geometries/TextGeometry.js";
-
-//load fonts
-const normalFont = './GeosansLight_regular.json';
-const alienFont = './Sith AF_Regular.json';
 
 //scene comp and renderer config
 const scene = new THREE.Scene();
@@ -30,7 +24,7 @@ const bloomPass = new UnrealBloomPass(
   0.3
 );
 bloomPass.threshold = 0;
-bloomPass.strength = 0.6;
+bloomPass.strength = 0.8;
 bloomPass.radius = 1;
 const bloomComposer = new EffectComposer(renderer);
 bloomComposer.setSize(window.innerWidth, window.innerHeight);
@@ -46,28 +40,6 @@ function onMouseMove( event ) {
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 window.addEventListener( 'mousemove', onMouseMove, false );
-
-//loading texts
-function addTextOnPosition(text, xPos, yPos, zPos, fontName, fontSize = 10){
-  const fontloader = new FontLoader();
-  fontloader.load(fontName, function(font){
-  const textGeometry = new TextGeometry(text, {
-    font: font,
-    size: fontSize,
-    height: 2
-  });
-  const textMesh = new THREE.Mesh(textGeometry, new THREE.MeshStandardMaterial({
-    emissive: 0xffffff
-  }));
-  textMesh.position.set(xPos, yPos, zPos);
-  textMesh.lookAt(xPos,160,120);
-  textMesh.name="text";
-  scene.add(textMesh);
-  });
-}
-
-addTextOnPosition('Ciri -', -23, 60, 0, normalFont);
-addTextOnPosition('Sciri', 7, 60, 0, alienFont);
 
 //background
 const backGround = new THREE.TextureLoader().load('universebg.png');
@@ -102,7 +74,7 @@ function createFakePlanet(radius, image, normal){
 }
 
 const planetA = createPlanet(14, 'planetA.jpg', 'ANormalMap.png', 170, 'Zoyovoga');
-const planetB = createPlanet(12, 'planetB.png', 'BNormalMap.png', 210, 'Nesnope');
+const planetB = createPlanet(12, 'planetB.png', 'BNormalMap.png', 220, 'Nesnope');
 const planetC = createPlanet(4, 'planetC.png', 'CNormalMap.jpg', 120, 'Setune');
 const planetMuna = createPlanet(3.2, 'planetE.png', 'MunaNormal.png', 100, 'Muna');
 const planetD = createPlanet(3, 'planetD.png', 'DNormalMap.png', 75, 'Roinides');
@@ -110,13 +82,13 @@ const planetD = createPlanet(3, 'planetD.png', 'DNormalMap.png', 75, 'Roinides')
 //Sun
 const geometrySun = new THREE.SphereGeometry(38,32,32);
 const materialSun = new THREE.MeshStandardMaterial({
-  emissiveMap: new THREE.TextureLoader().load('star.jpg'),
-  emissive: 0xff9999
+  emissiveMap: new THREE.TextureLoader().load('star2.jpg'),
+  emissive: 0xffffff
 });
 const sun = new THREE.Mesh(geometrySun,materialSun);
 sun.name = 'Ciri';
 sun.position.set(0,0,0);
-sun.rotateX(Math.PI/1.4);
+sun.lookAt(0,120,160);
 scene.add(sun);
 
 //light
@@ -153,9 +125,8 @@ function animate(){
   planetC_angle += 0.00022;
   translatePlanet(planetC, 120, planetC_angle);
 
-  planetD.rotation.y += 0.0135;
-  planetD_angle += 0.0005;
-  translatePlanet(planetD, 75, planetD_angle);
+  planetD_angle += 0.00055;
+  translatePlanet(planetD, 60, planetD_angle);
 
   planetMuna.rotation.y += 0.016;
   planetMuna_angle += 0.0003;
@@ -168,7 +139,7 @@ function animate(){
 	raycaster.setFromCamera( mouse, camera );
   // calculate objects intersecting the picking ray
 	const intersects = raycaster.intersectObjects( scene.children );
-  //remover rings
+  //remover rings ou fake planets
   scene.traverse( function( node ) {
     if ( node instanceof THREE.Mesh ) {
         if(node.name == "select")
@@ -180,9 +151,8 @@ function animate(){
   //add rings arround meshes
 	for ( let i = 0; i < intersects.length; i ++ ) {
 		if(intersects[ i ].object.name !== 'text' && intersects[ i ].object.name !== 'fakeplanet'){
-      console.log(intersects[ i ].object.name);
       let radius = intersects[ i ].object.geometry.parameters.radius && intersects[ i ].object.geometry.parameters.radius > 8 ? intersects[ i ].object.geometry.parameters.radius: 8;
-      const geometry = new THREE.RingGeometry( radius*1.4, radius*1.5, 1024 );
+      const geometry = new THREE.RingGeometry( radius*1.4, radius*1.4 + 2, 1024 );
       const material = new THREE.MeshBasicMaterial( { color: 0xffffff, side: THREE.DoubleSide } );
       const mesh = new THREE.Mesh( geometry, material );
       mesh.position.set(intersects[ i ].object.position.x, intersects[ i ].object.position.y, intersects[ i ].object.position.z);
@@ -193,6 +163,8 @@ function animate(){
 	}
 }
 
+const planetView = false;
+
 function translatePlanet(mesh, radius, angle){
   mesh.position.set(radius * Math.cos(angle), 0, radius * Math.sin(angle))
 }
@@ -201,12 +173,89 @@ function onPlanetClick( event ){
   raycaster.setFromCamera( mouse, camera );
 	const intersects = raycaster.intersectObjects( scene.children );
   for ( let i = 0; i < intersects.length; i ++ ){
-    console.log(intersects[ i ].object.material.map.image);
-    const fakeplanet = createFakePlanet(100, intersects[ i ].object.material.map.image.src, intersects[ i ].object.material.normalMap.image.src);
+    if(planetView || intersects[ i ].object.name == "fakeplanet"){
+      return;
+    }
+    if(intersects[ i ].object.name == "Ciri"){
+      createFakePlanet(100, 'star2.jpg', 'starNormalMap1.png');
+      bloomPass.strength = 1.4;
+    }
+    else{
+      createFakePlanet(100, intersects[ i ].object.material.map.image.src, intersects[ i ].object.material.normalMap.image.src);
+      bloomPass.strength = 0.2;
+    }
     camera.position.set(1200,0,0);
     camera.lookAt(1000,0,0);
-    bloomPass.strength = 0;
     ambient_light.intensity = 1;
+    writeFields(intersects[ i ].object.name)
+  }
+  planetView = true;
+}
+
+function writeFields(name){
+  document.getElementById('name').innerHTML = name;
+  document.getElementById('name').hidden = false;
+  document.getElementById('type').hidden = false
+  document.getElementById('diameter').hidden = false;
+  document.getElementById('mass').hidden = false;
+  document.getElementById('temperature').hidden = false;
+  document.getElementById('moons').hidden = false;
+  document.getElementById('habitability').hidden = false;
+  document.getElementById('description').hidden = false;
+   
+  if(name == 'Ciri'){
+    document.getElementById('type').innerHTML = 'Star Type: Class M (Red Dwarf)'
+    document.getElementById('diameter').innerHTML = 'Diameter: 278536 km'
+    document.getElementById('mass').innerHTML = 'Mass: 8.7492e+29 kg'
+    document.getElementById('temperature').innerHTML = 'Surface Temperature: 3560 K'
+    document.getElementById('moons').hidden = true;
+    document.getElementById('habitability').hidden = true;
+    document.getElementById('description').innerHTML = 'Description: Red Dwarves are the most common stars in the universe. Their low luminosity means they are difficult to observe with the naked eye from afar. Although they typically have an extremely long lifespan, red dwarves emit almost no UV light resulting in unfavorable conditions for most forms of life.'
+  }
+  else if(name == 'Zoyovoga'){
+    document.getElementById('type').innerHTML = 'Planet Type: Gas Gigant'
+    document.getElementById('diameter').innerHTML = 'Diameter: 140338 km'
+    document.getElementById('mass').innerHTML = 'Mass: 1.9982e+29 kg'
+    document.getElementById('temperature').innerHTML = 'Surface Temperature: 38.15 K'
+    document.getElementById('moons').innerHTML = 'Moons: 41'
+    document.getElementById('habitability').innerHTML = 'Habitability: 0%';
+    document.getElementById('description').innerHTML = 'Description: Zoyovoga is a gas giant planet mostly composed of helium and hydrogen swirling above its solid core. Its greenish blue color is due to the presence of ice, methane and ammonia.'
+  }
+  else if(name == 'Nesnope'){
+    document.getElementById('type').innerHTML = 'Planet Type: Gas Gigant'
+    document.getElementById('diameter').innerHTML = 'Diameter: 121916 km'
+    document.getElementById('mass').innerHTML = 'Mass: 1.6531e+27 kg'
+    document.getElementById('temperature').innerHTML = 'Surface Temperature: 44.2 K'
+    document.getElementById('moons').innerHTML = 'Moons: 32'
+    document.getElementById('habitability').innerHTML = 'Habitability: 0%';
+    document.getElementById('description').innerHTML = 'Description: Nesnope is a gas giant planet mostly composed of helium and hydrogen swirling above its solid core. Its light pink color is due to the high presence of water and methane clouds that greatly reflects Ciri\'s red light.'
+  }
+  else if(name == 'Setune'){
+    document.getElementById('type').innerHTML = 'Planet Type: Tundra World'
+    document.getElementById('diameter').innerHTML = 'Diameter: 20161 km'
+    document.getElementById('mass').innerHTML = 'Mass: 9.9822e+24 kg'
+    document.getElementById('temperature').innerHTML = 'Surface Temperature: 180.2 K'
+    document.getElementById('moons').innerHTML = 'Moons: 2'
+    document.getElementById('habitability').innerHTML = 'Habitability: 20%';
+    document.getElementById('description').innerHTML = 'Description: Setune is a cold and rocky world with a nitrogen-oxygen atmosphere. Permafrost covers most of the surface and a stable biosphere exists but vegetation is mostly limited to mosses and lichens.'
+  }
+  else if(name == 'Muna'){
+    document.getElementById('type').innerHTML = 'Planet Type: Desert World'
+    document.getElementById('diameter').innerHTML = 'Diameter: 14777 km'
+    document.getElementById('mass').innerHTML = 'Mass: 6.1432e+24 kg'
+    document.getElementById('temperature').innerHTML = 'Surface Temperature: 228.25 K'
+    document.getElementById('moons').innerHTML = 'Moons: 1'
+    document.getElementById('habitability').innerHTML = 'Habitability: 80%';
+    document.getElementById('description').innerHTML = 'Description: Muna is a desert world composed almost entirely by platinum silicon dioxide dunes and shallow salty water oceans, and its atmosfere is rich in nitrogen that gives a very pink sky throughout the day. Muna is home of the Pukhu\'a, the only lifeform on the planet.'
+  }
+  else if(name == 'Roinides'){
+    document.getElementById('type').innerHTML = 'Planet Type: Desert World'
+    document.getElementById('diameter').innerHTML = 'Diameter: 10777 km'
+    document.getElementById('mass').innerHTML = 'Mass: 3.9932e+24 kg'
+    document.getElementById('temperature').innerHTML = 'Surface Temperature: 278.25 K - 50.2 K'
+    document.getElementById('moons').innerHTML = 'Moons: 0'
+    document.getElementById('habitability').innerHTML = 'Habitability: 0%';
+    document.getElementById('description').innerHTML = 'Description: Roinides is a dry, rocky world with almost no atmosphere. The dust-covered terrain consists largely of mesas, canyons and mountains. Temperature between day and night sides varies significantly since the planet is tidal locked.'
   }
 }
 
@@ -224,8 +273,18 @@ window.addEventListener(
       });
       camera.position.set(0,160,120);
       camera.lookAt(0,0,0);
-      bloomPass.strength = 0.6;
+      bloomPass.strength = 0.8;
       ambient_light.intensity = 0;
+      document.getElementById('name').hidden = true;
+      document.getElementById('type').hidden = true;
+      document.getElementById('diameter').hidden = true;
+      document.getElementById('mass').hidden = true;
+      document.getElementById('temperature').hidden = true;
+      document.getElementById('moons').hidden = true;
+      document.getElementById('habitability').hidden = true;
+      document.getElementById('description').hidden = true;
+
+      planetView = false;
     }
 });
 
